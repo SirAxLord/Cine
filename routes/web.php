d1b74b1c098ba0b91362fd8f802abfb3b5589c01
SirAxLord <A325641@alumnos.uaslp.mx>
2025-10-17 19:45:07 -0600

    CRUD creado de Salas

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 use App\Http\Controllers\adminController;
 use App\Http\Controllers\sucursalesController;
+use App\Http\Controllers\salasController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -35,11 +36,20 @@ Route::middleware(['auth'])->group(function () {
         )
         ->name('two-factor.show');
 
-        Route::get('sucursales', [sucursalesController::class, 'index'])->name('sucursales.index');
-    Route::post('sucursales/save', [sucursalesController::class, 'save'])->name('sucursales.save');
-    Route::match(['put','patch'], 'sucursales/update/{id}', [sucursalesController::class, 'update'])->name('sucursales.update');
+        //Rutas de Sucursales
+        Route::get('sucursales/index', [sucursalesController::class, 'index'])->name('sucursales.index');
+        Route::post('sucursales/save', [sucursalesController::class, 'save'])->name('sucursales.save');
+        Route::match(['put','patch'], 'sucursales/update/{id}', [sucursalesController::class, 'update'])->name('sucursales.update');
         Route::delete('sucursales/delete/{id}', [sucursalesController::class, 'delete'])->name('sucursales.delete');
-    Route::get('sucursales/modifica/{id}', [sucursalesController::class, 'show'])->name('sucursales.show');
+        Route::get('sucursales/modifica/{id}', [sucursalesController::class, 'show'])->name('sucursales.show');
+
+        //Rutas de Salas
+        Route::get('salas/index', [salasController::class, 'index'])->name('salas.index');
+        Route::post('salas/save', [salasController::class, 'save'])->name('salas.save');
+        Route::match(['put','patch'], 'salas/update/{id}', [salasController::class, 'update'])->name('salas.update');
+        Route::delete('salas/delete/{id}', [salasController::class, 'delete'])->name('salas.delete');
+        Route::get('salas/modifica/{id}', [salasController::class, 'show'])->name('salas.show');
+
 });
 
 require __DIR__.'/auth.php';
